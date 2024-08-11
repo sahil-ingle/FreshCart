73a57cdfe9b8cd224bdd8d60212b4528d0807e9c
sahil-ingle <inglesahil00@gmail.com>
2024-08-11 20:19:28 +0530

    update 0.2

--- app/src/main/java/com/example/freshcart/LoginPage.java
+++ app/src/main/java/com/example/freshcart/LoginPage.java
@@ -3,13 +3,17 @@ package com.example.freshcart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.freshcart.databinding.ActivityLoginPageBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     private ActivityLoginPageBinding binding;
 
@@ -26,7 +30,43 @@ public class LoginPage extends AppCompatActivity {
             finish();
         });
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        binding.loginBtn.setOnClickListener(v -> {
+            String mEmail = binding.email.getText().toString().trim();
+            String mPass = binding.password.getText().toString().trim();
+
+            if (validateInput(mEmail,mPass)){
+                login(mEmail,mPass);
+            }
+        });
+
     }
 
+    private void login(String email, String password){
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, task ->{
+            if (task.isSuccessful()){
+                startActivity(new Intent(LoginPage.this, HomePage.class));
+                finish();
+            }else{
+                Toast.makeText(LoginPage.this, "password wrong", Toast.LENGTH_LONG).show();
+            }
+        });
 
+    }
+
+    private boolean validateInput(String email,String pass){
+        if (email.isEmpty()){
+            Toast.makeText(LoginPage.this, "Enter email", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(pass.isEmpty()){
+            Toast.makeText(LoginPage.this, "Enter Password", Toast.LENGTH_LONG).show();
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
--- app/src/main/java/com/example/freshcart/RegisterPage.java
+++ app/src/main/java/com/example/freshcart/RegisterPage.java
@@ -3,15 +3,19 @@ package com.example.freshcart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.freshcart.databinding.ActivityRegisterPageBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterPage extends AppCompatActivity {
 
     private ActivityRegisterPageBinding binding;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,43 @@ public class RegisterPage extends AppCompatActivity {
             overridePendingTransition(0, 0);
             finish();
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.registerBtn.setOnClickListener(v -> {
+            String mEmail = binding.email.getText().toString().trim();
+            String mPassword = binding.password.getText().toString().trim();
+            String mConfirmPass = binding.confirmPass.getText().toString().trim();
+
+            if (validateDetails(mEmail, mPassword, mConfirmPass)) {
+                signUp(mEmail, mPassword);
+            }
+        });
+
     }
+
+    private void signUp(String email, String pass) {
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                startActivity(new Intent(this, HomePage.class));
+                finish();
+            } else {
+                Toast.makeText(RegisterPage.this, "Wrong credietnails", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean validateDetails(String email, String pass, String confrimPass) {
+        if (email.isEmpty() || pass.isEmpty() || confrimPass.isEmpty()){
+            Toast.makeText(RegisterPage.this, "Empty Credentials", Toast.LENGTH_LONG).show();
+            return false;
+        }else if (!pass.matches(confrimPass)){
+            Toast.makeText(RegisterPage.this, "Password doesn't Match", Toast.LENGTH_LONG).show();
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
 }
