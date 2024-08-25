b968e378794fe6d04cdee0b73bb2b6480b4ed4cd
sahil-ingle <inglesahil00@gmail.com>
2024-08-25 19:55:26 +0530

    update 0.4

--- app/src/main/java/com/example/freshcart/HomePage.java
+++ app/src/main/java/com/example/freshcart/HomePage.java
@@ -3,8 +3,12 @@ package com.example.freshcart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.freshcart.R;
+
 import com.example.freshcart.databinding.ActivityHomePageBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,26 +28,32 @@ public class HomePage extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        binding.logOutBtn.setOnClickListener(v -> {
-            mAuth.signOut();
+        replaceFragment(new HomeFragment());
 
-            if (mAuth.getCurrentUser() == null) {
-                Intent intent = new Intent(this, LoginPage.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish();
-            } else {
-                Toast.makeText(HomePage.this, "Something went wrong, try again", Toast.LENGTH_SHORT).show();
-            }
-
-        });
 
         binding.profileBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, ProfilePage.class);
             startActivity(intent);
         });
 
+        binding.navBar.setOnItemSelectedListener( item -> {
+
+            if (item.getItemId() == R.id.home){
+                replaceFragment(new HomeFragment());
+            }else if ( item.getItemId() == R.id.settings){
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+
     }
 
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
 
 }
